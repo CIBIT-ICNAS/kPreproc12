a2c66c7c46b8510e4af871261eec0eb8b6aa9260
kbraunlich <kbraunlich@gmail.com>
2015-07-31 16:26:33 -0400

    Update a__PREPROC.m

--- a__PREPROC.m
+++ a__PREPROC.m
@@ -13,7 +13,7 @@ spm_jobman('initcfg');
 
 %% Settings:
 % -----------------------------------------
-subs=[5 7:18];                                
+subs=[3:5 7:21 24:26 101];
 runs=[1:3];                                  
 
 nDropped=3;                                 % nVolumes dropped from beginning of each run
@@ -24,8 +24,6 @@ nSlices=33;                                 %
 TA=TR-(TR/nSlices);                         %
 refSlice=19;                                % 
 
-fwhm=6;                                     % smoothing (scalar --assumes iso)
-
 % slice order -----------------
 sliceOrder=[1:2:nSlices 2:2:nSlices];       % ascending interleaved
 % sliceOrder=[nSlices:-2:1,nSlices-1:-2:1];   % descending interleaved
@@ -33,11 +31,17 @@ sliceOrder=[1:2:nSlices 2:2:nSlices];       % ascending interleaved
 % sliceOrder=nSlices:-1:1;                    % descending
 % -----------------------------
 
+% smoothing:
+fwhm=6;                                     % smoothing (scalar --assumes iso)
+% segmentation:
+seg.template= 'eastern';                    % 'eastern' or 'mni'
+seg.saveDeformFields = [1 1];               % need at least [0 1] for this step.
+% housecleaning:
 ave_anat=2;                                 % 1= create averaged anat
 q=2;                                        % 1= quit at end to allow sleep
 
 home=pwd;
-mriFldr='/Volumes/Sarapiqui/valueAcmltr/mriData';
+mriFldr='/Volumes/Sarapiqui/valueAcmltr/mriData/spm12';
 script_fldr='/Volumes/Sarapiqui/valueAcmltr/scripts';
 preproc_fldr=[ script_fldr '/kPreproc12' ];
 addpath(preproc_fldr);
@@ -55,19 +59,20 @@ b_batch_stc(subs,runs,mriFldr,nFiles,refSlice,sliceOrder,TR,nSlices,TA)
 disp('stc done')
 close all
 
-%% MC (estimate and reslice)
-% -----------------------------------------
+% %% MC (estimate and reslice)
+% % -----------------------------------------
 c_batch_mc(subs,runs,mriFldr,nFiles)
 disp('mc done')
-
-%% coregistration
-% -----------------------------------------
+% 
+% %% coregistration
+% % -----------------------------------------
 d_batch_coreg(subs,runs,mriFldr,nFiles,0,0)
 disp('coreg done')
 
+
 %% anatomical segmentation
 % -----------------------------------------
-e_batch_seg(subs,mriFldr,0)
+e_batch_seg(subs,mriFldr,0,seg)
 disp('seg done')
 
 %% skull strip
@@ -90,18 +95,24 @@ disp('deform funcs done')
 i_batch_smooth(subs,runs,mriFldr,nFiles,fwhm);
 disp('smooth done')
 
-%% quality assessment
+%% quality control
 % -----------------------------------------
 kSpmSwitch(8)
 addpath(genpath('/Users/kurtb/Dropbox/kMat/tor/diagnostics'))
+addpath(genpath('/Users/kurtb/Dropbox/kMat/KSPM/spm_u'))
+
 for sub=subs
     for run=runs
         j_kQuality(mriFldr,sub, run)
+        j_checkNorm(mriFldr,sub,run);
     end
+    j_spike(mriFldr,sub,runs)
+    cd(preproc_fldr)
 end
 close all
 disp('quality assessment done')
 kSpmSwitch(12)
+
 %% compress folders
 % -----------------------------------------
 k_batch_zip(subs,runs,mriFldr)
@@ -112,15 +123,15 @@ l_batch_checkReg(subs,runs,mriFldr,nFiles)
 
 %% create mean anatomical
 % -----------------------------------------
+% placed here for convenience. check qc first
 if ave_anat==1
     cd(mriFldr)
     cd ..
-    spm_mean_ui
-    V=spm_vol('mean.img');
-    ima=spm_read_vols(V);
-    delete('mean.img','mean.hdr')
-    V.fname='mean.nii';
-    spm_write_vol(V,ima);
+    for i=1:numel(subs)
+        sub=subs(i);
+        imgs(i,:)=filenames([mriFldr sprintf('/s%3.3d/anat/wmasked_*.nii',sub)],'char')
+    end
+    spm_mean(imgs)
 end
 %%
 toc
