41a2d7f2dee6f0f7cc276c7d26843f862958afa7
kbraunlich <kbraunlich@gmail.com>
2015-07-31 16:29:43 -0400

    Update g_batch_deformAnat.m

--- g_batch_deformAnat.m
+++ g_batch_deformAnat.m
@@ -1,22 +1,26 @@
 
 
-function g_batch_deformAnat(subs,~,home_path)
+function g_batch_deformAnat(subs,~,mriFldr)
 
-script_path=cd;
 
 
 for s=1:numel(subs)
-   sub=subs(s);
-   anat_dfrm=dir([home_path '/s' sprintf('%3.3d',sub) '/anat/y_s' sprintf('%3.3d',sub) '*.nii']);
-   cd( [home_path '/s' sprintf('%3.3d',sub) '/anat']);
-   matlabbatch{1}.spm.util.defs.comp{1}.def = {...
-      [home_path '/s' sprintf('%3.3d',sub) '/anat/' anat_dfrm.name]};
-   matlabbatch{1}.spm.util.defs.ofname = '';
-   matlabbatch{1}.spm.util.defs.fnames = {[home_path '/s' sprintf('%3.3d',sub) ...
-      '/anat/masked_s' sprintf('%3.3d',sub) '.nii,1']};
-   matlabbatch{1}.spm.util.defs.savedir.savepwd = 1;
-   matlabbatch{1}.spm.util.defs.interp = 3;
-   spm_jobman('run', matlabbatch);
+    sub=subs(s);
+    anat_dfrm=filenames([mriFldr '/s' sprintf('%3.3d',sub) '/anat/y_s' sprintf('%3.3d',sub) '*.nii'],'char');
+    maskedAnat=filenames([mriFldr '/s' sprintf('%3.3d',sub) '/anat/mask*.nii'],'char');
+    
+    
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def = cellstr(anat_dfrm);
+    matlabbatch{1}.spm.spatial.normalise.write.subj.resample = cellstr(maskedAnat);
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+        78 76 85];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+    
+    
+    
+    
+
+    spm_jobman('run', matlabbatch);
 end
 
-cd(script_path)
