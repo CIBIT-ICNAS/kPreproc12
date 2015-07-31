84b352c16a773de82a2aceee18e72bc12aeaa1ed
kbraunlich <kbraunlich@gmail.com>
2015-07-31 16:29:23 -0400

    Update f_batch_create_maskedAnat.m

--- f_batch_create_maskedAnat.m
+++ f_batch_create_maskedAnat.m
@@ -1,22 +1,22 @@
-function f_batch_create_maskedAnat(subs,home_path)
+function f_batch_create_maskedAnat(subs,mriFldr)
 
 script_path=cd;
 for s=1:numel(subs)
    sub=subs(s);
-   file1=dir([home_path '/s' sprintf('%3.3d',sub) '/anat/ms' sprintf('%3.3d',sub) '*.nii']);
-   file2=dir([home_path '/s' sprintf('%3.3d',sub) '/anat/c1s' sprintf('%3.3d',sub) '*.nii']);
-   file3=dir([home_path '/s' sprintf('%3.3d',sub) '/anat/c2s' sprintf('%3.3d',sub) '*.nii']);
-   file4=dir([home_path '/s' sprintf('%3.3d',sub) '/anat/c3s' sprintf('%3.3d',sub) '*.nii']);
+   file1=dir([mriFldr '/s' sprintf('%3.3d',sub) '/anat/s' sprintf('%3.3d',sub) '*.nii']);
+   file2=dir([mriFldr '/s' sprintf('%3.3d',sub) '/anat/c1s' sprintf('%3.3d',sub) '*.nii']);
+   file3=dir([mriFldr '/s' sprintf('%3.3d',sub) '/anat/c2s' sprintf('%3.3d',sub) '*.nii']);
+   file4=dir([mriFldr '/s' sprintf('%3.3d',sub) '/anat/c3s' sprintf('%3.3d',sub) '*.nii']);
    
    matlabbatch{1}.spm.util.imcalc.input = {
-      [home_path '/s' sprintf('%3.3d',sub) '/anat/' file1.name ',1']
-      [home_path '/s' sprintf('%3.3d',sub) '/anat/' file2.name ',1']
-      [home_path '/s' sprintf('%3.3d',sub) '/anat/' file3.name ',1']
-      [home_path '/s' sprintf('%3.3d',sub) '/anat/' file4.name ',1']
+      [mriFldr '/s' sprintf('%3.3d',sub) '/anat/' file1.name ',1']
+      [mriFldr '/s' sprintf('%3.3d',sub) '/anat/' file2.name ',1']
+      [mriFldr '/s' sprintf('%3.3d',sub) '/anat/' file3.name ',1']
+      [mriFldr '/s' sprintf('%3.3d',sub) '/anat/' file4.name ',1']
       };
    
    matlabbatch{1}.spm.util.imcalc.output = ['masked_s' sprintf('%3.3d',sub) '.nii'];
-   matlabbatch{1}.spm.util.imcalc.outdir = {[home_path '/s' sprintf('%3.3d',sub) '/anat/']};
+   matlabbatch{1}.spm.util.imcalc.outdir = {[mriFldr '/s' sprintf('%3.3d',sub) '/anat/']};
    matlabbatch{1}.spm.util.imcalc.expression = 'i1.*(i2+i3+i4)';
    matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
    matlabbatch{1}.spm.util.imcalc.options.mask = 0;
